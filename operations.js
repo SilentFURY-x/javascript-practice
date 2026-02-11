375c84a961c2ca8accff63d29f33d04812ef4b54
SilentFURY-x <tyagiarjun1809gmail.com>
2026-02-11 23:33:52 +0530

    Operations

---
+++ operations.js
@@ -0,0 +1,23 @@
+let val = 3
+let negval = -val
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)  
+
+
+// console.log("2" + 2)
+// console.log("2" + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(true)
+// console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
