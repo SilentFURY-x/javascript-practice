96ebec6f9eed94ebec07c5c143a19e1f370fabff
SilentFURY-x <tyagiarjun1809gmail.com>
2026-02-11 23:22:05 +0530

    Conversion of datatypes

--- conversion.js
+++ conversion.js
@@ -8,3 +8,7 @@ let valueInNumber = Number(score)
 // null to number = 0
 // string to number = NaN (if not a proper number)
 // undefined to number = NaN
+// 1 to boolean = true
+// 0 to boolean = false
+// "" to boolean = false
+// "hello" to boolean = true
