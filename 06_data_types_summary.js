646b82ae854335bf3cf28f5ebcb696d22c4f501b
SilentFURY-x <tyagiarjun1809gmail.com>
2026-02-12 00:20:33 +0530

    06_DataTypes_Summary

---
+++ 06_data_types_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types :
+// 1. string
+// 2. number
+// 3. bigint
+// 4. boolean
+// 5. undefined
+// 6. symbol
+// 7. null
+
+
+// Reference (Non-Primitive)
+
+// Arrays, Objects, Functions
+
+// Array
+
+const arr = ["Hello", "World"]
+
+// Object
+const obj = {
+  greeting: "Hello",
+  location: "World"
+}
+
+// Function
+const myFunction = function() {
+    console.log("Hello, World!");
+}
+
+console.log(typeof arr) // object
+console.log(typeof obj) // object
+console.log(typeof myFunction) // function
