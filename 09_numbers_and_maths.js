4051ba630deed395cdb39f91fe2187bee11c30d8
SilentFURY-x <tyagiarjun1809gmail.com>
2026-02-15 15:12:33 +0530

    09_numbers_maths

---
+++ 09_numbers_and_maths.js
@@ -0,0 +1,34 @@
+// const sccore  = 400
+// console.log(sccore)
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+// const anotherNumber = 23.4567
+// console.log(anotherNumber.toPrecision(4)) // returns a string
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// +++++++++++++++++++++++MATHS+++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4, 2, 6, 8))
+// console.log(Math.max(4, 2, 6, 8))
+
+
+// console.log(Math.random()) // betwenn 0 to 1
+
+console.log(Math.floor((Math.random() * 10) + 1))
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
